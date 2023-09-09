10cbc5e6a71a1fcfca3da7dcd4c6b3c7b4cb77ed
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-09-09 17:29:11 -0500

    Remove greetings and content

--- webpack.config.js
+++ webpack.config.js
@@ -40,11 +40,7 @@ var options = {
   entry: {
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
-    contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
     panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
-  },
-  chromeExtensionBoilerplate: {
-    notHotReload: ['contentScript'],
   },
   output: {
     filename: '[name].bundle.js',
@@ -151,15 +147,6 @@ var options = {
               })
             );
           },
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/pages/Content/content.styles.css',
-          to: path.join(__dirname, 'build'),
-          force: true,
         },
       ],
     }),
