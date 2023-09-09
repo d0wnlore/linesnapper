4e59a717a0fce9945ca4dcae3debc63e923aeeb2
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-09-09 17:26:54 -0500

    Remove background task

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,11 @@ var options = {
   entry: {
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
-    background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
     panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
   },
   chromeExtensionBoilerplate: {
-    notHotReload: ['background', 'contentScript'],
+    notHotReload: ['contentScript'],
   },
   output: {
     filename: '[name].bundle.js',
